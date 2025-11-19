65f594722a3411ff822d31e3ae90ce47b8e16464
Felipe <hoffmannfelipe@gmail.com>
2025-11-19 11:25:48 -0300

    feat: implementado hash de senhas para maior segurança

--- models/models.py
+++ models/models.py
@@ -1,9 +1,36 @@
-from db.db import db
+from db.db import db # Assumindo que você tem isso
+import hashlib
+import os
+import binascii
 from flask_login import UserMixin
 
-class Usuario(UserMixin ,db.Model):
-    __tablename__ = 'usuarios'
-
+class Usuario(UserMixin, db.Model):
+    __tablename__ = 'usuarios' # ou o nome da sua tabela
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(40),unique=True)
-    senha = db.Column(db.String())
+    nome = db.Column(db.String(100), unique=True, nullable=False)
+    # Aumente o tamanho do campo 'senha' para armazenar o salt + hash (ex: 128 ou 255)
+    senha = db.Column(db.String(255), nullable=False) 
+
+    def set_password(self, password):
+        """Hashea a senha e armazena o salt+hash no campo senha."""
+        salt = os.urandom(16)
+        hashed_password = hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            salt,
+            100000
+        )
+        # Armazena salt e hash combinados
+        self.senha = binascii.hexlify(salt).decode('utf-8') + binascii.hexlify(hashed_password).decode('utf-8')
+
+    def check_password(self, password):
+        """Verifica a senha fornecida contra o hash armazenado."""
+        salt = binascii.unhexlify(self.senha[:32])
+        stored_hash = binascii.unhexlify(self.senha[32:])
+        provided_hash = hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            salt,
+            100000
+        )
+        return hashlib == stored_hash, provided_hash
--- routes/user_route.py
+++ routes/user_route.py
@@ -1,26 +1,22 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, redirect, render_template, request, url_for, flash
 from db.db import db
-from models.models import Usuario
+from models.models import Usuario 
 from flask_login import LoginManager, login_user, login_required, logout_user
-
 
 
 usuario_bp = Blueprint('usuario_bp', __name__)
 lm = LoginManager()
 lm.init_app(usuario_bp)
 
-
-
 @lm.user_loader
 def user_loader(id):
-    usuario = db.session.query(Usuario).filter_by(id=id).first()
-    return usuario
+    user = db.session.query(Usuario).filter_by(id=id).first()
+    return user
 
 @usuario_bp.route('/')
 @login_required
 def home():
     return render_template('home.html')
-
 
 
 @usuario_bp.route('/register', methods=['GET', 'POST'])
@@ -30,13 +26,16 @@ def register():
     elif request.method == 'POST':
         nome = request.form['nomeForm']
         senha = request.form['senhaForm']
-        new_user = Usuario(nome=nome, senha=(senha))
+        
+        new_user = Usuario(nome=nome)
+ 
+        new_user.set_password(senha) 
+
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
         return redirect(url_for('usuario_bp.home'))
     
-
 
 @usuario_bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -46,15 +45,19 @@ def login():
     nome = request.form['nomeForm']
     senha = request.form['senhaForm']
 
-    user = Usuario.query.filter_by(nome=nome, senha=senha).first()
+    user = Usuario.query.filter_by(nome=nome).first()
 
     if not user:
-        return 'Usuário não cadastrado'
+        return 'Usuário não cadastrado ou senha incorreta'
 
-    if user.senha != senha:
+
+    if user.check_password(senha):
+        login_user(user)
+
+        return redirect(url_for('usuario_bp.home'))
+    else:
+
         return 'Senha incorreta'
-    login_user(user)
-    return redirect(url_for('usuario_bp.home'))
 
 
 @usuario_bp.route('/logout')
@@ -62,4 +65,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('usuario_bp.home'))
-
