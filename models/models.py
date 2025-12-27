3074cd5d2cd39d4caeb8f4d5b4ace4566e830f09
Felipe <hoffmannfelipe@gmail.com>
2025-12-27 11:38:14 -0300

    Pequenos ajustes na função de salvar senhas em hash

--- models/models.py
+++ models/models.py
@@ -1,18 +1,17 @@
-from db.db import db # Assumindo que você tem isso
+from db.db import db 
 import hashlib
 import os
 import binascii
 from flask_login import UserMixin
 
 class Usuario(UserMixin, db.Model):
-    __tablename__ = 'usuarios' # ou o nome da sua tabela
+    __tablename__ = 'usuarios' 
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), unique=True, nullable=False)
-    # Aumente o tamanho do campo 'senha' para armazenar o salt + hash (ex: 128 ou 255)
+ 
     senha = db.Column(db.String(255), nullable=False) 
 
     def set_password(self, password):
-        """Hashea a senha e armazena o salt+hash no campo senha."""
         salt = os.urandom(16)
         hashed_password = hashlib.pbkdf2_hmac(
             'sha256',
@@ -20,11 +19,11 @@ class Usuario(UserMixin, db.Model):
             salt,
             100000
         )
-        # Armazena salt e hash combinados
+  
         self.senha = binascii.hexlify(salt).decode('utf-8') + binascii.hexlify(hashed_password).decode('utf-8')
 
     def check_password(self, password):
-        """Verifica a senha fornecida contra o hash armazenado."""
+      
         salt = binascii.unhexlify(self.senha[:32])
         stored_hash = binascii.unhexlify(self.senha[32:])
         provided_hash = hashlib.pbkdf2_hmac(
